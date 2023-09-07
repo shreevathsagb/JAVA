166f27e73e21edf65abfad1bd9aae7528ea1545f
shreevathsagb <108cs21047@gmail.com>
2023-09-07 20:59:26 +0530

    fifth commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String args[]){
-        System.out.println("Hello Kuvempunagar")
-        System.out.println("CPC Polytechnic")        
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b)        
     }
 
 }
